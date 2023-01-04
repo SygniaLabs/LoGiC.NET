13b9f4cb3b447ec6883dd8f94c4510bb78e7d902
DoronV <119859363+dvazgiel@users.noreply.github.com>
2023-01-04 16:59:57 +0200

    Remove drand and drop, add help msg, extern all protection to args, remove watermark, remove readkey

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using dnlib.DotNet;
+using dnlib.DotNet.Writer;
 using LoGiC.NET.Protections;
 using SharpConfigParser;
-using LoGiC.NET.Utils;
-using dnlib.DotNet.Writer;
 
 namespace LoGiC.NET
 {
@@ -22,11 +22,35 @@ namespace LoGiC.NET
 
         public static MemoryStream Stream = new MemoryStream();
 
-        static void Main(string[] _)
+        static void Main(string[] args)
         {
-            Console.WriteLine("- Drag & drop your file:");
-            string path = Console.ReadLine().Replace("\"", string.Empty);
+            //Console.WriteLine("- Drag & drop your file:");
+            //string path = Console.ReadLine().Replace("\"", string.Empty);
+            
+            if (args.Length == 0 || args.Contains("?"))
+            {             
+                string helpMsg = "Usage: LoGiC.NET [.NET binary] optionA optionB optionN\n" +
+                "?                 - Show this message\n" +
+                "Renamer            - rename types, methods and their parameters, properties, fields and events to random strings.\n" +
+                "AntiTamper         - take the MD5 of the target, put it as a byte, and write it at the EndOfFile.Then a method is injected in the GlobalType, also knwon as < 'Module' >.\n" +
+                "JunkDefs           - add random junk defs to make the code harder to decrypt.\n" +
+                "StringEncryption   - string encryption.\n" +
+                "AntiDe4dot         - prevents usage of De4Dot (an open source .NET deobfuscator and unpacker).\n" +
+                "ControlFlow        - rearranges the instructions in a method to make the flow of control more difficult to follow.\n" +
+                "IntEncoding        - encodes the integers within different methods.\n" +
+                "ProxyAdder         - The intensity of the proxy calls. The more the intensity is, the more proxy calls will be added.\n" +
+                "InvalidMetadata    - add addtional jusnk methods and nasted types.\n";
+                Console.WriteLine(helpMsg);
+                return;                
+            }
+            string path = args[0];
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Unable to locate file in: " + path);
+                return;
+            }
 
+            path = Path.GetFullPath(path);
             Console.WriteLine("- Preparing obfuscation...");
             if (!File.Exists("config.txt"))
             {
@@ -62,15 +86,24 @@ namespace LoGiC.NET
 
             foreach (Protection protection in protections)
             {
-                Console.WriteLine("- Executing protection: " + protection.Name);
-                protection.Execute();
+                protection.Name = protection.Name.Replace("-", "").ReplaceSpaces();
+                if (args.Contains(protection.Name, StringComparer.OrdinalIgnoreCase)) 
+                {
+                    Console.WriteLine("- Executing protection: " + protection.Name);
+                    protection.Execute();
+                }
+                else
+                {
+                    Console.WriteLine("- Skipping protection: " + protection.Name);
+                }
             }
 
-            Console.WriteLine("- Watermarking...");
-            Watermark.AddAttribute();
+            Console.WriteLine("- Skipping Watermarking...");
+            //Watermark.AddAttribute();
 
-            Console.WriteLine("- Saving file...");
-            FilePath = @"C:\Users\" + Environment.UserName + @"\Desktop\" + Path.GetFileNameWithoutExtension(path) + "_protected" + FileExtension;
+            
+            FilePath = Path.GetDirectoryName(path) + Path.DirectorySeparatorChar + "protected_" + Path.GetFileName(path);
+            Console.WriteLine("- Saving file: " + FilePath);
             Module.Write(Stream, new ModuleWriterOptions(Module) { Logger = DummyLogger.NoThrowInstance });
 
             Console.WriteLine("- Stripping DOS header...");
@@ -82,8 +115,8 @@ namespace LoGiC.NET
             if (AntiTamper.Tampered)
                 AntiTamper.Inject(FilePath);
 
-            Console.WriteLine("- Done! Press any key to exit...");
-            Console.ReadKey();
+            //Console.WriteLine("- Done! Press any key to exit...");
+            //Console.ReadKey();
         }
     }
 }
